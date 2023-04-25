8483d105831dd01a1ddff00d3fa9b8ede93facc1
Liquid Monks <info@liquidmonks.com>
2023-04-25 14:31:37 -0500

    Application includes 3rd-party tailwindcss component.

--- apps/lp/src/pages/index.tsx
+++ apps/lp/src/pages/index.tsx
@@ -1,16 +1,21 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from "react";
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import tw from "tailwind-styled-components";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 const headingString: string = "Welcome to Bus Booker";
+
+// Use PascalCase for component names and change 'title' to 'Title'
+const Title = tw.h1`text-red-500 uppercase`;
+
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" description={undefined} children={undefined} />
-    <h1 className="text-red-500">{headingString}</h1>
+    <Title>{headingString}</Title>
   </Layout>
-)
+);
 
 export default IndexPage;
