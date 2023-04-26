7d5ea7a8680c1705719818d6df61b729bc970fed
Liquid Monks <info@liquidmonks.com>
2023-04-26 17:35:44 -0500

    Landing page includes a hero image.

--- apps/lp/src/pages/index.tsx
+++ apps/lp/src/pages/index.tsx
@@ -3,14 +3,14 @@ import {Layout, Seo} from "../components";
 import tw from "tailwind-styled-components";
 
 
-const headingString: string = "Welcome to Bus Booker";
 
-const Title = tw.h1`text-red-500 uppercase`;
+const Title = tw.h1`text-5xl text-red-500 uppercase`;
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" description={undefined} children={undefined} />
-    <Title>{headingString}</Title>
+    <Title>{ "The Elite Bus Co."}</Title>
+    <img src="https://images.unsplash.com/photo-1597920467799-ec8bee99f6eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=640&q=80" alt="busses" />
   </Layout>
 );
 
