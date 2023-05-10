63228635bb8df7695db96958492fbe66679ff4d6
Liquid Monks <info@liquidmonks.com>
2023-05-10 09:57:43 -0500

    The commit includes a section folder that compartmentalizes all landing page sections to their own respective folders.

--- apps/lp/src/pages/index.tsx
+++ apps/lp/src/pages/index.tsx
@@ -1,105 +1,12 @@
 import React from "react";
 import {Layout, Seo} from "../components";
-import tw from "tailwind-styled-components";
-
-const Title = tw.span`text-5xl text-green-500 uppercase font-bold font-title`;
-const Caption = tw.span`text-3xl text-gray-900 uppercase font-bold font-title`;
-const Description = tw.h1`text-2xl text-gray-500`;
-const MainImage = tw.img`rounded-3xl shadow-lg`;
-
-const Section = tw.div`max-w-screen-2xl mx-auto relative`
-const Separator = tw.div`w-10 h-1 bg-blue-500`
-
-const Button = tw.button`bg-green-500 px-5 py-3 rounded-xl text-white font-bold uppercase`
-const Gradient = tw.div`absolute inset-0 bg-gradient-radial-hero`
-const BookingFormDummyDiv = tw.div`bg-white h-24 w-2/3 rounded-3xl shadow-xl`
-
-const heroFeatures = [
-  {
-    title: "Book Easy",
-    description: "Tickets with QR Code"
-  },
-  {
-    title: "Discounts",
-    description: "For those who create an account"
-  },
-  {
-    title: "30+",
-    description: "Cities around the U.S."
-  }
-]
-
-const HeroFeatureItem = ({ item }) => (
-  <div>
-    <h5 className="font-bold">{item.title}</h5>
-    <p>{item.description}</p>
-    </div>
-)
-
-const HeroFeatures = () => (
-  <div className="flex gap-4">
-{heroFeatures.map((item) => (
-  <HeroFeatureItem item={item} />
-))}
-  </div>
-)
-
-const LogoTitle = () => (
-  <div>
-    <div>
-    <Caption>{"The Elite"}</Caption>
-    <div className="flex mt-1">
-      <Title className="text-blue-500">{"Bus"}</Title>
-      <Title className="ml-3 text-green-500 ">{"Company"}</Title>
-    </div>
-    </div>
-  <Separator className="mt-4" />
-  </div>
-)
-
-const TextSection = () => (
-  <div>
-  <div className="space-y-6" >    
-  <LogoTitle />
-  <Description>
-    {
-      "Experience the ultimate in luxury and convenience on the open road with The Elite Bus Company's high-end fleet, state-of-the-art amenities, and commitment to safety."
-    }
-  </Description>
-  <Button>{"Create Account"}</Button>
-  </div>
-  <div className="mt-32">
-  <HeroFeatures />
-  </div>
-  </div>
-)
-
-const GradientBackground = () => (
- <div className="absolute top-0 left-0 right-0 h-[900px] z-0" >
-  <Gradient />
- </div>
-)
-
-const BookingForm = () => (
-  <div className="flex justify-center mt-16">
-    <BookingFormDummyDiv />
-  </div>
-)
+import { HeroSection } from "../sections";
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" description={undefined} children={undefined} meta={undefined} />
-    <GradientBackground />
-    <Section>
-      <BookingForm />
-      <div className="flex items-center justify-between mt-16 gap-x-8">
-   <TextSection />
-    <div>
-    <MainImage src="https://images.unsplash.com/photo-1570125909994-e06d01047c28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="busses" />
-  </div>
-  </div>
-  </Section>
-  </Layout>
-);
+    <Seo title="Home" meta={undefined} children={undefined} />
+    <HeroSection />
+    </Layout>
+    )
 
 export default IndexPage;
